12b0c02e522e212ec74432a4b1d3639da33d1879
MitroxinPavel <mitrokhinpavel@gmail.com>
2023-12-02 20:53:29 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getName());
+        System.out.println("Сравнение бинов HelloWorld: " + (bean1 == bean2));
+        System.out.println("Сравнение бинов Cat: " + (bean3 == bean4));
+    }
+}
